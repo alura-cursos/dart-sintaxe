8777626269deec81fc51e195ade892f4aca13fbe
Ricarth Lima <rrsl@cin.ufpe.br>
2022-06-06 19:28:07 -0300

    aula02

--- bin/dart_sintaxe.dart
+++ bin/dart_sintaxe.dart
@@ -1,7 +1,51 @@
 void main(List<String> arguments) {
   Viajar jalapao = Viajar(locomocao: Transporte.carro);
-  jalapao.aventura();
-  jalapao.aventura2();
+  jalapao.aventura(); // codigo sem enum
+  jalapao.aventura2(); // codigo com enum
+
+  List<String> destinos = [
+    'Jalapão',
+    'Chapada dos Veadeiros',
+    'João Pessoa',
+    'Chapada dos Veadeiros'
+  ];
+  print(destinos);
+
+  Set destinosVisitados = <String>{
+    'Rio de Janeiro',
+    'Chapada dos Veadeiros',
+    'São Paulo',
+    'Chapada dos Veadeiros',
+  };
+  print(destinosVisitados);
+
+  Set destinosDesejados = <String>{
+    'Jalapão',
+    'Califórnia',
+    'Gramado',
+    'Chapada Diamantina',
+    'São Paulo'
+  };
+  print(destinosDesejados.intersection(destinosVisitados));
+
+  Map<String, String> motivoMap = {
+    'Jalapão': 'Natureza',
+    'Califórnia': 'Arte',
+    'Gramado': 'Chocolate',
+    'Chapada Diamantina': 'Cachoeiras',
+    'São Paulo': 'Compras',
+  };
+
+  print(motivoMap);
+
+  Map<String, dynamic> precoMap = {
+    'Jalapão': 2380.00,
+    'Califórnia': 'MUITO CARO',
+    'Gramado': 1200,
+    'Chapada Diamantina': 600,
+    'São Paulo': 'Barato',
+  };
+  print(precoMap);
 }
 
 class Viajar {
